c343066dc70a6956d792bb8b12a07d073bd42545
dgrald <grald.dylan@gmail.com>
2016-10-27 20:05:11 -0600

    update state correctly when selecting new point for line

--- __tests__/reducers/NewLocationSelectReducer.test.js
+++ __tests__/reducers/NewLocationSelectReducer.test.js
@@ -6,20 +6,58 @@ import AddLocationButtonClassReducer from '../../src/reducers/AddLocationButtonC
 describe("NewLocationSelectReducer", () => {
 
   let newLocationButtonClass = "new-class";
+  let description = "description";
+  let newLocation = L.latLng(50.5, 30.5);
 
   AddLocationButtonClassReducer.getNewClass.mockImplementation(() =>  {
     return newLocationButtonClass;
   });
 
-  describe("select new Point", () => {
+  describe("select Point", () => {
     it("should return new state with selectlocation", () => {
-      let newLocation = L.latLng(50.5, 30.5);
-      let description = "description";
       let inputState = {description: description, selectlocation: null, postButtonClass: "disabled", markers: []};
 
       let newState = NewLocationSelectReducer.getNewState(inputState, newLocation);
 
       let expectedState = {selectlocation: newLocation, postButtonClass: newLocationButtonClass, markers: [{location: {coordinates : [newLocation.lng, newLocation.lat], type: "Point"}}]};
+      expect(newState).toEqual(expectedState);
+      expect(AddLocationButtonClassReducer.getNewClass).toBeCalledWith(description, newLocation);
+    });
+  });
+
+  describe("select LineString", () => {
+
+    it("should return correct GeoJSON when adding first point for a new line", () => {
+      let inputState = {description: description, mode: "LineString", selectlocation: null, postButtonClass: "something", markers: null};
+
+      let newState = NewLocationSelectReducer.getNewState(inputState, newLocation);
+
+      let expectedState = {
+        selectlocation: newLocation,
+        postButtonClass: newLocationButtonClass,
+        markers: [{location: {coordinates : [[newLocation.lng, newLocation.lat]], type: "LineString"}}]
+      };
+      expect(newState).toEqual(expectedState);
+      expect(AddLocationButtonClassReducer.getNewClass).toBeCalledWith(description, newLocation);
+    });
+
+    it("should return correct GeoJSON when adding new point to existing line", () => {
+      let location2 = L.latLng(51.5, 31.5);
+      let inputState = {
+        description: description,
+        mode: "LineString",
+        selectlocation: [newLocation],
+        postButtonClass: "something",
+        markers: [{location: {coordinates : [[newLocation.lng, newLocation.lat]], type: "LineString"}}]
+      };
+
+      let newState = NewLocationSelectReducer.getNewState(inputState, location2);
+
+      let expectedState = {
+        selectlocation: location2,
+        postButtonClass: newLocationButtonClass,
+        markers: [{location: {coordinates : [[newLocation.lng, newLocation.lat], [location2.lng, location2.lat]], type: "LineString"}}]
+      };
       expect(newState).toEqual(expectedState);
       expect(AddLocationButtonClassReducer.getNewClass).toBeCalledWith(description, newLocation);
     });
--- src/reducers/NewLocationSelectReducer.js
+++ src/reducers/NewLocationSelectReducer.js
@@ -4,11 +4,25 @@ import AddLocationButtonClassReducer from './AddLocationButtonClassReducer';
 
 var NewLocationSelectReducer = assign({}, EventEmitter.prototype, {
   getNewState: function(oldState, locationSelect) {
-    return {
-      selectlocation: locationSelect,
-      postButtonClass: AddLocationButtonClassReducer.getNewClass(oldState.description, locationSelect),
-      markers: [{location: {coordinates : [locationSelect.lng, locationSelect.lat], type: "Point"}}]
-    };
+    let newPostButtonClass = AddLocationButtonClassReducer.getNewClass(oldState.description, locationSelect);
+    let newCoordinates = [locationSelect.lng, locationSelect.lat];
+    switch (oldState.mode) {
+      case "LineString":
+        let updatedCoordinates = (oldState.markers && oldState.markers.length) ?
+          oldState.markers[0].location.coordinates.concat([newCoordinates])
+          : [newCoordinates];
+        return {
+          selectlocation: locationSelect,
+          postButtonClass: newPostButtonClass,
+          markers: [{location: {coordinates : updatedCoordinates, type: oldState.mode}}]
+        };
+      default:
+        return {
+          selectlocation: locationSelect,
+          postButtonClass: newPostButtonClass,
+          markers: [{location: {coordinates : newCoordinates, type: "Point"}}]
+        };
+    }
   }
 });
 
